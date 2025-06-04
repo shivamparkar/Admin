3a2fa2c25126a738c3e5d96b7e51d7cfba7cbc82
Shivam <parkarshivam@gmail.com>
2025-06-04 15:05:45 +0530

    ..

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,14 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   base: './',
+   build: {
+    rollupOptions: {
+      output: {
+        assetFileNames: "assets/[name]-[hash].[ext]",
+        entryFileNames: "assets/[name]-[hash].js",
+        chunkFileNames: "assets/[name]-[hash].js"
+      }
+    }
+  },
   plugins: [react()],
 })
